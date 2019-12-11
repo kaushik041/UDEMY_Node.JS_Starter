6c6b52881cd59e9773b1a5e61aa1a534016ab048
kaushik roy <kaushik.roypara@gmail.com>
2019-12-11 18:17:08 +0530

    data import from file to mongo

---
+++ dev-data/data/tour-dev-data.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const fs = require('fs');
+const dotenv = require('dotenv')
+dotenv.config({path: './config.env'});
+const Tour = require('./../../models/tourModel');
+
+//console.log(process.env.NODE_ENV);
+const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology: true,
+    useFindAndModify:false
+}).then(() => {
+    //console.log(con.connections);
+    console.log('Database Connection installed successfully');
+});
+
+//Read JSON file
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`,'utf-8'));
+
+//Import file data to MongoDB
+const tourImport = async() =>{
+    try{
+        await Tour.create(tours);
+        console.log('Data Imported to Database');
+        process.exit();
+    }catch(err){
+        console.log(err);
+    }
+}
+
+//Delete data from database
+const tourRemove = async() =>{
+    try{
+        await Tour.deleteMany(tours);
+        console.log('Data removed from Database');
+        process.exit();
+    }catch(err){
+        console.log(err);
+    }
+}
+
+if(process.argv[2] === '--import'){
+    tourImport();
+}else if(process.argv[2] === '--delete'){
+    tourRemove();
+}
--- models/tourModel.js
+++ models/tourModel.js
@@ -4,16 +4,52 @@ const tourSchema = new mongoose.Schema({
     name:{
         type:String,
         required:[true, 'name field required'],
-        unique:true
+        //unique:true
     },
-    rating:{
+    duration:{
+        type:Number,
+        required:[true, 'duration field required']
+    },
+    maxGroupSize:{
+        type:Number,
+        required:[true, 'max group size is required']
+    },
+    difficulty:{
+        type:String,
+        required:[true, 'difficulty field required']
+    },
+    ratingsAverage:{
         type:Number,
         default: 4.5
+    },
+    ratingsQuantity:{
+        type:Number,
+        default:0
     },
     price:{
         type:Number,
         required:[true, 'price field required']
-    }
+    },
+    priceDiscount:Number,
+    summary:{
+        type:String,
+        trim:true,
+        required:[true,'Summary field required']
+    },
+    description:{
+        type:String,
+        trim:true
+    },
+    imageCover:{
+        type:String,
+        required:[true,'Cover image required']
+    },
+    images:[String],
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    },
+    startDates:[Date]
 });
 
 module.exports = mongoose.model('Tour', tourSchema);
