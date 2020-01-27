ec2095f9c54ef419d372f620b37b65320bc8b429
kaushik roy <kaushik.roypara@gmail.com>
2020-01-27 17:56:17 +0530

    update password

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,5 +6,6 @@ router.post('/singup', authController.signup);
 router.post('/login', authController.login);
 router.post('/forget-password', authController.forgetPassword);
 router.patch('/reset-password/:token', authController.resetPassowrd);
+router.patch('/update-password',authController.protectRoute, authController.updatePassword);
 
 module.exports = router;
